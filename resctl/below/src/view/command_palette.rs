e227a6b53ee51c0b4c95eb1d168aea4db37d61ef
Boyu Ni <boyuni@fb.com>
2020-07-15 13:27:25 -0700

    View warning framework
    Summary: Added view warning framework
    
    Reviewed By: dschatzberg
    
    Differential Revision: D22465188
    
    fbshipit-source-id: 628692a270c5395c76d90ea570739bade494639d

--- resctl/below/src/view/command_palette.rs
+++ resctl/below/src/view/command_palette.rs
@@ -12,9 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use cursive::theme::ColorStyle;
 use cursive::vec::Vec2;
 use cursive::Printer;
 use cursive::View;
+
+/// Command palette will have different mode:
+/// Info is used to show info like full cgroup path.
+/// Alert is used to show error messages.
+// TODO: command mode for command palette.
+#[derive(PartialEq)]
+enum CPMode {
+    Info,
+    Alert,
+}
 
 /// TextView that used to display extra information
 ///
@@ -22,28 +33,75 @@ use cursive::View;
 /// full cgroup name. But the idea for this view is something like vim's command palette
 /// that use for input operation command like search, filter, rearrange, apply config, etc.
 pub struct CommandPalette {
-    info: String,
+    content: String,
+    mode: CPMode,
 }
 
 impl View for CommandPalette {
     fn draw(&self, printer: &Printer) {
         printer.print_hline((0, 0), printer.size.x, "─");
-        printer.print((0, 1), &self.info);
+        // Message should adapt the screen size
+        let mut msg_len_left = self.content.len();
+        let mut idx = 0;
+        let mut line = 1;
+        while msg_len_left > printer.size.x {
+            self.print(printer, (0, line), idx);
+            msg_len_left -= printer.size.x;
+            idx += printer.size.x;
+            line += 1;
+        }
+        self.print(printer, (0, line), idx);
     }
 
-    fn required_size(&mut self, _constraint: Vec2) -> Vec2 {
-        Vec2::new(1, 2)
+    fn required_size(&mut self, constraint: Vec2) -> Vec2 {
+        Vec2::new(1, self.content.len() / constraint.x + 2)
     }
 }
 
 impl CommandPalette {
     /// Create a new CommandPalette
-    pub fn new<T: Into<String>>(info: T) -> Self {
-        Self { info: info.into() }
+    pub fn new<T: Into<String>>(content: T) -> Self {
+        Self {
+            content: content.into(),
+            mode: CPMode::Info,
+        }
     }
 
     /// Set the display info
-    pub fn set_info<T: Into<String>>(&mut self, info: T) {
-        self.info = info.into();
+    pub fn set_info<T: Into<String>>(&mut self, content: T) {
+        self.content = content.into();
+        self.mode = CPMode::Info;
+    }
+
+    /// Set alert
+    /// This will preempt the command palette mode.
+    pub fn set_alert<T: Into<String>>(&mut self, content: T) {
+        self.content = content.into();
+        self.mode = CPMode::Alert;
+    }
+
+    fn print_info(&self, printer: &Printer, pos: Vec2, idx: usize) {
+        if idx + printer.size.x > self.content.len() {
+            printer.print(pos, &self.content[idx..]);
+        } else {
+            printer.print(pos, &self.content[idx..idx + printer.size.x]);
+        }
+    }
+
+    fn print_alert(&self, printer: &Printer, pos: Vec2, idx: usize) {
+        printer.with_color(ColorStyle::title_primary(), |printer| {
+            if idx + printer.size.x > self.content.len() {
+                printer.print(pos, &self.content[idx..]);
+            } else {
+                printer.print(pos, &self.content[idx..idx + printer.size.x]);
+            }
+        })
+    }
+
+    fn print<T: Into<Vec2>>(&self, printer: &Printer, pos: T, idx: usize) {
+        match self.mode {
+            CPMode::Info => self.print_info(printer, pos.into(), idx),
+            CPMode::Alert => self.print_alert(printer, pos.into(), idx),
+        }
     }
 }
