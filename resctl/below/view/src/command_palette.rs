bc7de00c1c851f8bda9f13bed3fea186786bfb20
Boyu Ni <boyuni@fb.com>
2020-09-29 11:40:37 -0700

    Below command palette implementation: Support cmd history
    Summary: Support up/down to iterate cmd history for cmd palette. By default, below will cache recent 10 cmds.
    
    Reviewed By: dschatzberg
    
    Differential Revision: D23600067
    
    fbshipit-source-id: 28791ba8121e2db55924a041566d756835335060

--- resctl/below/view/src/command_palette.rs
+++ resctl/below/view/src/command_palette.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use cursive::event::{Event, EventResult};
+use cursive::event::{Event, EventResult, Key};
 use cursive::theme::ColorStyle;
 use cursive::vec::Vec2;
 use cursive::views::{EditView, NamedView};
@@ -21,11 +21,13 @@ use cursive::Printer;
 use cursive::View;
 
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::rc::Rc;
 
 use crate::controllers::Controllers;
 use crate::stats_view::{StatsView, ViewBridge};
+
+const MAX_CMD_HISTORY: usize = 10;
 
 /// Command palette will have different mode:
 /// Info is used to show info like full cgroup path.
@@ -50,6 +52,8 @@ pub struct CommandPalette {
     mode: CPMode,
     cmd_view: RefCell<EditView>,
     cmd_controllers: Rc<RefCell<HashMap<&'static str, Controllers>>>,
+    cmd_history: VecDeque<String>,
+    cur_cmd_idx: usize,
 }
 
 impl View for CommandPalette {
@@ -84,7 +88,17 @@ impl View for CommandPalette {
     }
 
     fn on_event(&mut self, event: Event) -> EventResult {
-        self.cmd_view.borrow_mut().on_event(event)
+        match event {
+            Event::Key(Key::Up) => {
+                self.prev_cmd();
+                EventResult::Consumed(None)
+            }
+            Event::Key(Key::Down) => {
+                self.next_cmd();
+                EventResult::Consumed(None)
+            }
+            _ => self.cmd_view.borrow_mut().on_event(event),
+        }
     }
 
     fn required_size(&mut self, constraint: Vec2) -> Vec2 {
@@ -105,10 +119,55 @@ impl CommandPalette {
             mode: CPMode::Info,
             cmd_view: RefCell::new(
                 EditView::new()
-                    .on_submit(move |c, cmd| Self::run_cmd::<V>(name, c, cmd))
+                    .on_submit(move |c, cmd| {
+                        Self::handle_cmd_history(name, c, cmd);
+                        Self::run_cmd::<V>(name, c, cmd)
+                    })
                     .style(ColorStyle::terminal_default()),
             ),
             cmd_controllers,
+            cmd_history: VecDeque::new(),
+            cur_cmd_idx: 0,
+        }
+    }
+
+    fn handle_cmd_history(name: &'static str, c: &mut Cursive, cmd: &str) {
+        c.call_on_name(
+            &format!("{}_cmd_palette", name),
+            |cp: &mut NamedView<CommandPalette>| {
+                let mut cmd_palette = cp.get_mut();
+                cmd_palette.cmd_history.push_back(cmd.into());
+                if cmd_palette.cmd_history.len() > MAX_CMD_HISTORY {
+                    cmd_palette.cmd_history.pop_front();
+                }
+                cmd_palette.cur_cmd_idx = cmd_palette.cmd_history.len() - 1;
+            },
+        );
+    }
+
+    fn prev_cmd(&mut self) {
+        if self.cmd_history.is_empty() {
+            return;
+        }
+        self.cmd_view
+            .borrow_mut()
+            .set_content(&self.cmd_history[self.cur_cmd_idx]);
+        if self.cur_cmd_idx > 0 {
+            self.cur_cmd_idx -= 1;
+        }
+    }
+
+    fn next_cmd(&mut self) {
+        if self.cmd_history.is_empty() {
+            return;
+        }
+        if self.cur_cmd_idx == self.cmd_history.len() - 1 {
+            self.cmd_view.borrow_mut().set_content("");
+        } else {
+            self.cur_cmd_idx += 1;
+            self.cmd_view
+                .borrow_mut()
+                .set_content(&self.cmd_history[self.cur_cmd_idx]);
         }
     }
 
