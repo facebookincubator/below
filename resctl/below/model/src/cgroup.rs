e5541db2225736ff21e2ab71ec68f658732f149f
Leon Yang <lnyng@fb.com>
2021-01-05 08:45:22 -0800

    Make models queriables
    Summary:
    Currently below model derives BelowDecor, which uses proc_macro to generate getters for all fields. There are multiple drawbacks. To list a few:
    - We can't see actual implementations. Relying on comment to guess the getter behaviors (especially the styled ones) is hard. This adds difficulty to debug and adding new features.
    - It's hard to enumerate fields. We need a huge match statement for all fields every time we want to do some per-field operations, or we need to add more proc_macro, leading to proliferation.
    - Rendering attributes are tightly coupled with the model. Title, width, highlight etc. are irrelavent to the model. To reduce boilerplates we have to couple them with the model definitions.
    
    This diff create a different mechanism to access different fields from the model in a unified way: the Field enum. It wraps the basic types we used in the model. With a Queriable trait that all models should implement, we can iterate over fields easily. Therefore, we only need to write the boilerplate match statement a few times to completely avoid using proc_macro. In fact, these boilerplate match statements are also easy to be created in proc_macro if want, i.e. create enum for each pub field in a Model struct. (Side note: there is a "structural" crate that does something similar but it's not popular).
    
    This diff only implement the Queriable trait for cgroup related models to showcase (following diffs will demostrate how this works altogether). Will implement other models later.
    
    Reviewed By: dschatzberg
    
    Differential Revision: D24962365
    
    fbshipit-source-id: e24958dce35460a1d8c9e3d6b901d5ff5ee69745

--- resctl/below/model/src/cgroup.rs
+++ resctl/below/model/src/cgroup.rs
@@ -162,6 +162,68 @@ impl CgroupModel {
     }
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub enum CgroupModelFieldId {
+    Name,
+    FullPath,
+    InodeNumber,
+    Cpu(CgroupCpuModelFieldId),
+    Mem(CgroupMemoryModelFieldId),
+    Io(CgroupIoModelFieldId),
+    Pressure(CgroupPressureModelFieldId),
+}
+
+impl FieldId for CgroupModelFieldId {
+    type Queriable = CgroupModel;
+}
+
+impl std::str::FromStr for CgroupModelFieldId {
+    type Err = ::strum::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use CgroupModelFieldId::*;
+        if let Some(s) = s.strip_prefix("cpu.") {
+            CgroupCpuModelFieldId::from_str(s).map(Cpu)
+        } else if let Some(s) = s.strip_prefix("mem.") {
+            CgroupMemoryModelFieldId::from_str(s).map(Mem)
+        } else if let Some(s) = s.strip_prefix("io.") {
+            CgroupIoModelFieldId::from_str(s).map(Io)
+        } else if let Some(s) = s.strip_prefix("pressure.") {
+            CgroupPressureModelFieldId::from_str(s).map(Pressure)
+        } else {
+            match s {
+                "name" => Ok(Name),
+                "full_path" => Ok(FullPath),
+                "inode_number" => Ok(InodeNumber),
+                _ => Err(Self::Err::VariantNotFound),
+            }
+        }
+    }
+}
+
+impl Queriable for CgroupModel {
+    type FieldId = CgroupModelFieldId;
+
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field> {
+        use CgroupModelFieldId::*;
+        match field_id {
+            Name => Some(Field::Str(self.name.clone())),
+            FullPath => Some(Field::Str(self.full_path.clone())),
+            InodeNumber => self.inode_number.map(Field::from),
+            Cpu(field_id) => self.cpu.as_ref().and_then(|m| m.query(field_id)),
+            Mem(field_id) => self.memory.as_ref().and_then(|m| m.query(field_id)),
+            Io(field_id) => self.io_total.as_ref().and_then(|m| m.query(field_id)),
+            Pressure(field_id) => self.pressure.as_ref().and_then(|m| m.query(field_id)),
+        }
+    }
+}
+
+impl Recursive for CgroupModel {
+    fn get_depth(&self) -> usize {
+        self.depth as usize
+    }
+}
+
 #[derive(Clone, Debug, Default, PartialEq, BelowDecor)]
 pub struct CgroupCpuModel {
     #[bttr(
@@ -209,6 +271,37 @@ impl CgroupCpuModel {
             nr_periods_per_sec: count_per_sec!(begin.nr_periods, end.nr_periods, delta),
             nr_throttled_per_sec: count_per_sec!(begin.nr_throttled, end.nr_throttled, delta),
             throttled_pct: usec_pct!(begin.throttled_usec, end.throttled_usec, delta),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum CgroupCpuModelFieldId {
+    UsagePct,
+    UserPct,
+    SystemPct,
+    NrPeriodsPerSec,
+    NrThrottledPerSec,
+    ThrottledPct,
+}
+
+impl FieldId for CgroupCpuModelFieldId {
+    type Queriable = CgroupCpuModel;
+}
+
+impl Queriable for CgroupCpuModel {
+    type FieldId = CgroupCpuModelFieldId;
+
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field> {
+        use CgroupCpuModelFieldId::*;
+        match field_id {
+            UsagePct => self.usage_pct.map(Field::from),
+            UserPct => self.user_pct.map(Field::from),
+            SystemPct => self.system_pct.map(Field::from),
+            NrPeriodsPerSec => self.nr_periods_per_sec.map(Field::from),
+            NrThrottledPerSec => self.nr_throttled_per_sec.map(Field::from),
+            ThrottledPct => self.throttled_pct.map(Field::from),
         }
     }
 }
@@ -281,6 +374,37 @@ impl std::ops::Add<&CgroupIoModel> for CgroupIoModel {
             wios_per_sec: opt_add(self.wios_per_sec, other.wios_per_sec),
             dbytes_per_sec: opt_add(self.dbytes_per_sec, other.dbytes_per_sec),
             dios_per_sec: opt_add(self.dios_per_sec, other.dios_per_sec),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum CgroupIoModelFieldId {
+    RbytesPerSec,
+    WbytesPerSec,
+    RiosPerSec,
+    WiosPerSec,
+    DbytesPerSec,
+    DiosPerSec,
+}
+
+impl FieldId for CgroupIoModelFieldId {
+    type Queriable = CgroupIoModel;
+}
+
+impl Queriable for CgroupIoModel {
+    type FieldId = CgroupIoModelFieldId;
+
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field> {
+        use CgroupIoModelFieldId::*;
+        match field_id {
+            RbytesPerSec => self.rbytes_per_sec.map(Field::from),
+            WbytesPerSec => self.wbytes_per_sec.map(Field::from),
+            RiosPerSec => self.rios_per_sec.map(Field::from),
+            WiosPerSec => self.wios_per_sec.map(Field::from),
+            DbytesPerSec => self.dbytes_per_sec.map(Field::from),
+            DiosPerSec => self.dios_per_sec.map(Field::from),
         }
     }
 }
@@ -558,6 +682,103 @@ impl CgroupMemoryModel {
     }
 }
 
+#[derive(PartialEq, Debug, Clone, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum CgroupMemoryModelFieldId {
+    Total,
+    Swap,
+    Anon,
+    File,
+    KernelStack,
+    Slab,
+    Sock,
+    Shmem,
+    FileMapped,
+    FileDirty,
+    FileWriteback,
+    AnonThp,
+    InactiveAnon,
+    ActiveAnon,
+    InactiveFile,
+    ActiveFile,
+    Unevictable,
+    SlabReclaimable,
+    SlabUnreclaimable,
+    Pgfault,
+    Pgmajfault,
+    WorkingsetRefault,
+    WorkingsetActivate,
+    WorkingsetNodereclaim,
+    Pgrefill,
+    Pgscan,
+    Pgsteal,
+    Pgactivate,
+    Pgdeactivate,
+    Pglazyfree,
+    Pglazyfreed,
+    ThpFaultAlloc,
+    ThpCollapseAlloc,
+    MemoryHigh,
+    EventsLow,
+    EventsHigh,
+    EventsMax,
+    EventsOom,
+    EventsOomKill,
+}
+
+impl FieldId for CgroupMemoryModelFieldId {
+    type Queriable = CgroupMemoryModel;
+}
+
+impl Queriable for CgroupMemoryModel {
+    type FieldId = CgroupMemoryModelFieldId;
+
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field> {
+        use CgroupMemoryModelFieldId::*;
+        match field_id {
+            Total => self.total.map(Field::from),
+            Swap => self.swap.map(Field::from),
+            Anon => self.anon.map(Field::from),
+            File => self.file.map(Field::from),
+            KernelStack => self.kernel_stack.map(Field::from),
+            Slab => self.slab.map(Field::from),
+            Sock => self.sock.map(Field::from),
+            Shmem => self.shmem.map(Field::from),
+            FileMapped => self.file_mapped.map(Field::from),
+            FileDirty => self.file_dirty.map(Field::from),
+            FileWriteback => self.file_writeback.map(Field::from),
+            AnonThp => self.anon_thp.map(Field::from),
+            InactiveAnon => self.inactive_anon.map(Field::from),
+            ActiveAnon => self.active_anon.map(Field::from),
+            InactiveFile => self.inactive_file.map(Field::from),
+            ActiveFile => self.active_file.map(Field::from),
+            Unevictable => self.unevictable.map(Field::from),
+            SlabReclaimable => self.slab_reclaimable.map(Field::from),
+            SlabUnreclaimable => self.slab_unreclaimable.map(Field::from),
+            Pgfault => self.pgfault.map(Field::from),
+            Pgmajfault => self.pgmajfault.map(Field::from),
+            WorkingsetRefault => self.workingset_refault.map(Field::from),
+            WorkingsetActivate => self.workingset_activate.map(Field::from),
+            WorkingsetNodereclaim => self.workingset_nodereclaim.map(Field::from),
+            Pgrefill => self.pgrefill.map(Field::from),
+            Pgscan => self.pgscan.map(Field::from),
+            Pgsteal => self.pgsteal.map(Field::from),
+            Pgactivate => self.pgactivate.map(Field::from),
+            Pgdeactivate => self.pgdeactivate.map(Field::from),
+            Pglazyfree => self.pglazyfree.map(Field::from),
+            Pglazyfreed => self.pglazyfreed.map(Field::from),
+            ThpFaultAlloc => self.thp_fault_alloc.map(Field::from),
+            ThpCollapseAlloc => self.thp_collapse_alloc.map(Field::from),
+            MemoryHigh => self.memory_high.map(Field::from),
+            EventsLow => self.events_low.map(Field::from),
+            EventsHigh => self.events_high.map(Field::from),
+            EventsMax => self.events_max.map(Field::from),
+            EventsOom => self.events_oom.map(Field::from),
+            EventsOomKill => self.events_oom_kill.map(Field::from),
+        }
+    }
+}
+
 fn is_pressure_significant(p: f64) -> Option<cursive::theme::BaseColor> {
     if p > 40.0 {
         Some(cursive::theme::BaseColor::Red)
@@ -621,6 +842,35 @@ impl CgroupPressureModel {
             io_full_pct: pressure.io.full.avg10,
             memory_some_pct: pressure.memory.some.avg10,
             memory_full_pct: pressure.memory.full.avg10,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum CgroupPressureModelFieldId {
+    CpuSomePct,
+    IoSomePct,
+    IoFullPct,
+    MemorySomePct,
+    MemoryFullPct,
+}
+
+impl FieldId for CgroupPressureModelFieldId {
+    type Queriable = CgroupPressureModel;
+}
+
+impl Queriable for CgroupPressureModel {
+    type FieldId = CgroupPressureModelFieldId;
+
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field> {
+        use CgroupPressureModelFieldId::*;
+        match field_id {
+            CpuSomePct => self.cpu_some_pct.map(Field::from),
+            IoSomePct => self.io_some_pct.map(Field::from),
+            IoFullPct => self.io_full_pct.map(Field::from),
+            MemorySomePct => self.memory_some_pct.map(Field::from),
+            MemoryFullPct => self.memory_full_pct.map(Field::from),
         }
     }
 }
--- resctl/below/model/src/lib.rs
+++ resctl/below/model/src/lib.rs
@@ -13,10 +13,12 @@
 // limitations under the License.
 
 use std::collections::{BTreeMap, BTreeSet};
+use std::fmt;
 use std::time::{Duration, Instant, SystemTime};
 
 use anyhow::{anyhow, bail, Context, Result};
 use cursive::utils::markup::StyledString;
+use strum_macros::EnumString;
 
 use below_derive::BelowDecor;
 use below_thrift::types::{CgroupSample, Sample, SystemSample};
@@ -34,6 +36,160 @@ pub use collector::*;
 pub use network::*;
 pub use process::*;
 pub use system::*;
+
+/// A wrapper for different field types used in Models. By this way we can query
+/// different fields in a single function without using Box.
+#[derive(Clone, Debug)]
+pub enum Field {
+    U64(u64),
+    I32(i32),
+    I64(i64),
+    F64(f64),
+    Str(String),
+}
+
+impl From<Field> for i64 {
+    fn from(field: Field) -> i64 {
+        match field {
+            Field::I32(v) => v as i64,
+            Field::I64(v) => v as i64,
+            _ => panic!("Operation for unsupported types"),
+        }
+    }
+}
+
+impl From<Field> for f64 {
+    fn from(field: Field) -> f64 {
+        match field {
+            Field::U64(v) => v as f64,
+            Field::I32(v) => v as f64,
+            Field::I64(v) => v as f64,
+            Field::F64(v) => v,
+            _ => panic!("Operation for unsupported types"),
+        }
+    }
+}
+
+impl From<Field> for String {
+    fn from(field: Field) -> String {
+        match field {
+            Field::Str(v) => v,
+            _ => panic!("Operation for unsupported types"),
+        }
+    }
+}
+
+impl From<u64> for Field {
+    fn from(v: u64) -> Self {
+        Field::U64(v)
+    }
+}
+
+impl From<i32> for Field {
+    fn from(v: i32) -> Self {
+        Field::I32(v)
+    }
+}
+
+impl From<i64> for Field {
+    fn from(v: i64) -> Self {
+        Field::I64(v)
+    }
+}
+
+impl From<f64> for Field {
+    fn from(v: f64) -> Self {
+        Field::F64(v)
+    }
+}
+
+impl From<String> for Field {
+    fn from(v: String) -> Self {
+        Field::Str(v)
+    }
+}
+
+impl std::ops::Add for Field {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (Field::U64(s), Field::U64(o)) => (s + o).into(),
+            (Field::I32(s), Field::I32(o)) => (s + o).into(),
+            (Field::I64(s), Field::I64(o)) => (s + o).into(),
+            (Field::F64(s), Field::F64(o)) => (s + o).into(),
+            (Field::Str(s), Field::Str(o)) => (s + &o).into(),
+            _ => panic!("Operation for unsupported types"),
+        }
+    }
+}
+
+impl PartialEq for Field {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Field::U64(s), Field::U64(o)) => s == o,
+            (Field::I32(s), Field::I32(o)) => s == o,
+            (Field::I64(s), Field::I64(o)) => s == o,
+            (Field::F64(s), Field::F64(o)) => s == o,
+            (Field::Str(s), Field::Str(o)) => s == o,
+            _ => false,
+        }
+    }
+}
+
+impl PartialOrd for Field {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Field::U64(s), Field::U64(o)) => s.partial_cmp(o),
+            (Field::I32(s), Field::I32(o)) => s.partial_cmp(o),
+            (Field::I64(s), Field::I64(o)) => s.partial_cmp(o),
+            (Field::F64(s), Field::F64(o)) => s.partial_cmp(o),
+            (Field::Str(s), Field::Str(o)) => s.partial_cmp(o),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Field {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Field::U64(v) => v.fmt(f),
+            Field::I32(v) => v.fmt(f),
+            Field::I64(v) => v.fmt(f),
+            Field::F64(v) => v.fmt(f),
+            Field::Str(v) => v.fmt(f),
+        }
+    }
+}
+
+/// Each Model is composed of Fields and optionally sub-Models. The Queriable
+/// trait let us query() a Model for a particular Field within the hierarchy
+/// with the given FieldId.
+pub trait Queriable {
+    type FieldId: FieldId<Queriable = Self>;
+    fn query(&self, field_id: &Self::FieldId) -> Option<Field>;
+}
+
+/// Marker trait to bind FieldId back to Queriable for type inference.
+pub trait FieldId: Sized {
+    type Queriable: Queriable<FieldId = Self> + ?Sized;
+}
+
+pub fn sort_queriables<T: Queriable>(queriables: &mut [&T], field_id: &T::FieldId, reverse: bool) {
+    queriables.sort_by(|lhs, rhs| {
+        let order = lhs
+            .query(field_id)
+            .partial_cmp(&rhs.query(field_id))
+            .unwrap_or(std::cmp::Ordering::Equal);
+        if reverse { order.reverse() } else { order }
+    });
+}
+
+/// Some Model are recursive, i.e. containing sub-Models with its own type. Such
+/// Model has a depth value for illustrating the tree hierarchy.
+pub trait Recursive {
+    fn get_depth(&self) -> usize;
+}
 
 pub struct Model {
     pub time_elapsed: Duration,
