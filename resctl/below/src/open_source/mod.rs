704e6c3ad0a87769689d9edd556cefba734d5c11
Daniel Xu <dxu@dxuuu.xyz>
2021-06-14 16:11:28 -0700

    bpf: Use build script instead of hacky cargo plugin (#8088)
    Summary:
    Now you can just run `cargo build` and things will "just work".
    
    Pull Request resolved: https://github.com/facebookincubator/below/pull/8088
    
    Reviewed By: brianc118
    
    Differential Revision: D29109102
    
    Pulled By: danobi
    
    fbshipit-source-id: b269be3b566e5c44653e6e693029b3571a6ebe80

--- resctl/below/src/open_source/mod.rs
+++ resctl/below/src/open_source/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[path = "../bpf/mod.rs"]
+#[path = "../bpf/.output/exitstat.skel.rs"]
 mod bpf;
 pub mod commands;
 pub mod init;
