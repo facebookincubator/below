e7aded45b387614aa97d695ef0a61f1934a31595
Brian Chen <brianc118@meta.com>
2023-06-22 14:13:01 -0700

    Support for offlined CPUs in /proc/stat (#8193)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/below/pull/8193
    
    Currently procfs assumes that no CPUs are offlined. That is not always
    the case.
    
    Addresses #8190.
    
    Reviewed By: antonis-m
    
    Differential Revision: D46937378
    
    fbshipit-source-id: 3f21fbd283fb43e4f6c64c4a28e6e756ca23a93e

--- below/procfs/src/types.rs
+++ below/procfs/src/types.rs
@@ -35,7 +35,9 @@ pub struct CpuStat {
 #[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Stat {
     pub total_cpu: Option<CpuStat>,
+    // TODO: deprecate in favor of cpus_map
     pub cpus: Option<Vec<CpuStat>>,
+    pub cpus_map: Option<BTreeMap<u32, CpuStat>>,
     pub total_interrupt_count: Option<u64>,
     pub context_switches: Option<u64>,
     pub boot_time_epoch_secs: Option<u64>,
