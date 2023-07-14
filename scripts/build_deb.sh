bb97c2dee0881022e55a570e368af7c114b11b3e
Daniel Xu <dxu@dxuuu.xyz>
2023-07-14 17:06:48 -0600

    scripts: build_deb.sh: Fix copyright header
    Copyright stuff usually goes before the comments.

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 #
-# This script spits out an ubuntu .deb package for below.
-#
-# For example, to build a .deb for ubuntu 18.04:
-#
-#     ./build_deb.sh 18.04
-#
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+# This script spits out an ubuntu .deb package for below.
+#
+# For example, to build a .deb for ubuntu 18.04:
+#
+#     ./build_deb.sh 18.04
+#
 
 set -eu
 
