2b00bef9e745884a6ab9ae508c8b1b69e86c6a0c
mmynk <mohit.ritanil@gmail.com>
2024-01-30 15:50:44 -0600

    Add sub-module for reading `tc` stats

---
+++ below/tc/src/lib.rs
@@ -0,0 +1,111 @@
+mod errors;
+mod types;
+
+#[cfg(test)]
+mod test;
+
+use std::collections::BTreeMap;
+
+use netlink_packet_core::{NetlinkHeader, NetlinkMessage, NetlinkPayload, NLM_F_DUMP, NLM_F_REQUEST};
+use netlink_packet_route::RouteNetlinkMessage;
+use netlink_packet_route::tc::TcMessage;
+use netlink_sys::constants::NETLINK_ROUTE;
+use netlink_sys::{Socket, SocketAddr};
+use nix::net::if_;
+
+pub use errors::*;
+pub use types::*;
+
+pub type TcStats = Vec<Tc>;
+pub type Result<T> = std::result::Result<T, TcError>;
+
+/// Get list of all `tc` qdiscs.
+pub fn tc_stats() -> Result<TcStats> {
+    let ifaces = get_interfaces()?;
+    read_tc_stats(ifaces, &get_netlink_qdiscs)
+}
+
+fn read_tc_stats(interfaces: BTreeMap<u32, String>, netlink_retriever: &dyn Fn() -> Result<Vec<TcMessage>>) -> Result<TcStats> {
+    let messages = netlink_retriever()?;
+    let tc_stats = messages
+        .into_iter()
+        .map(|msg| Tc::new(&interfaces, &msg))
+        .collect();
+
+    Ok(tc_stats)
+}
+
+/// Open a netlink socket to retrieve `tc` qdiscs.
+/// The underlying library sends a message of type `RTM_GETQDISC` to the kernel.
+/// The kernel responds with a message of type `RTM_NEWQDISC` for each qdisc.
+fn get_netlink_qdiscs() -> Result<Vec<TcMessage>> {
+    // open a socket
+    let socket = Socket::new(NETLINK_ROUTE).map_err(|e| TcError::Netlink(e.to_string()))?;
+    socket.connect(&SocketAddr::new(0, 0)).map_err(|e| TcError::Netlink(e.to_string()))?;
+
+    // create a netlink request
+    let mut nl_hdr = NetlinkHeader::default();
+    nl_hdr.flags = NLM_F_REQUEST | NLM_F_DUMP;
+    let msg = RouteNetlinkMessage::GetQueueDiscipline(TcMessage::default());
+    let mut packet = NetlinkMessage::new(nl_hdr, NetlinkPayload::from(msg));
+    packet.finalize();
+    let mut buf = vec![0; packet.header.length as usize];
+    packet.serialize(&mut buf[..]);
+
+    // send the request
+    socket.send(&buf[..], 0).map_err(|e| TcError::Netlink(e.to_string()))?;
+
+    // receive the response
+    let mut recv_buf = vec![0; 4096];
+    let mut offset = 0;
+    let mut response = Vec::new();
+    'out: while let Ok(size) = socket.recv(&mut &mut recv_buf[offset..], 0) {
+        loop {
+            let bytes = &recv_buf[offset..];
+            let rx_packet = <NetlinkMessage<RouteNetlinkMessage>>::deserialize(bytes)
+                .map_err(|e| TcError::Netlink(e.to_string()))?;
+            response.push(rx_packet.clone());
+            let payload = rx_packet.payload;
+            if let NetlinkPayload::Error(err) = payload {
+                return Err(TcError::Netlink(err.to_string()));
+            }
+            if let NetlinkPayload::Done(_) = payload {
+                break 'out;
+            }
+
+            offset += rx_packet.header.length as usize;
+            if offset == size || rx_packet.header.length == 0 {
+                offset = 0;
+                break;
+            }
+        }
+    }
+
+    let mut tc_msgs = Vec::new();
+    for msg in response {
+        if let NetlinkPayload::InnerMessage(RouteNetlinkMessage::NewQueueDiscipline(tc)) = msg.payload {
+            tc_msgs.push(tc);
+        }
+    }
+
+    return Ok(tc_msgs);
+}
+
+/// Get a map of interface index to interface name.
+fn get_interfaces() -> Result<BTreeMap<u32, String>> {
+    let ifaces = if_::if_nameindex().map_err(|e| TcError::ReadInterfaces(e.to_string()))?;
+    let if_map = ifaces
+    .iter()
+    .map(|iface| {
+        let index = iface.index();
+        let name = if let Ok(name) = iface.name().to_str() {
+            name.to_string()
+        } else {
+            String::new()
+        };
+        (index, name)
+    })
+    .collect::<BTreeMap<u32, String>>();
+
+    Ok(if_map)
+}
---
+++ below/tc/src/types.rs
@@ -0,0 +1,182 @@
+use std::collections::BTreeMap;
+
+use netlink_packet_route::tc;
+use netlink_packet_route::tc::{TcAttribute, TcFqCodelXstats, TcMessage, TcOption, TcQdiscFqCodelOption};
+use serde::{Deserialize, Serialize};
+
+const FQ_CODEL: &str = "fq_codel";
+
+/// `Tc` represents a traffic control qdisc.
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Tc {
+    pub if_index: u32,
+    pub if_name: String,
+    pub handle: u32,
+    pub parent: u32,
+    pub kind: String,
+    pub stats: Stats,
+    pub qdisc: Option<QDisc>,
+}
+
+impl Tc {
+    pub fn new(interfaces: &BTreeMap<u32, String>, tc_msg: &TcMessage) -> Self {
+        let if_index = tc_msg.header.index as u32;
+        let if_name = interfaces.get(&if_index).map_or_else(String::new, |iface| iface.to_string());
+        let mut tc = Self {
+            if_index,
+            if_name,
+            handle: tc_msg.header.handle.into(),
+            parent: tc_msg.header.parent.into(),
+            ..Default::default()
+        };
+        let mut opts = Vec::new();
+
+        for attr in &tc_msg.attributes {
+            match attr {
+                TcAttribute::Kind(name) => tc.kind = name.clone(),
+                TcAttribute::Options(tc_opts) => opts = tc_opts.to_vec(),
+                TcAttribute::Stats(tc_stats) => {
+                    tc.stats.bps = Some(tc_stats.bps);
+                    tc.stats.pps = Some(tc_stats.pps);
+                }
+                TcAttribute::Stats2(tc_stats) => {
+                    for stat in tc_stats {
+                        match stat {
+                            tc::TcStats2::Basic(basic) => {
+                                tc.stats.bytes = Some(basic.bytes);
+                                tc.stats.packets = Some(basic.packets);
+                            }
+                            tc::TcStats2::Queue(queue) => {
+                                tc.stats.qlen = Some(queue.qlen);
+                                tc.stats.backlog = Some(queue.backlog);
+                                tc.stats.drops = Some(queue.drops);
+                                tc.stats.requeues = Some(queue.requeues);
+                                tc.stats.overlimits = Some(queue.overlimits);
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                TcAttribute::Xstats(tc_xstats) => {
+                    match tc_xstats {
+                        tc::TcXstats::FqCodel(fq_codel_xstats) => {
+                            tc.stats.xstats = Some(XStats::FqCodel(FqCodelXStats::new(fq_codel_xstats)))
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+
+
+        tc.qdisc = QDisc::new(&tc.kind, opts);
+
+        tc
+    }
+}
+
+/// `Stats` represents the statistics of a qdisc.
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Stats {
+    // Stats2::StatsBasic
+    pub bytes: Option<u64>,
+    pub packets: Option<u32>,
+
+    // Stats2::StatsQueue
+    pub qlen: Option<u32>,
+    pub backlog: Option<u32>,
+    pub drops: Option<u32>,
+    pub requeues: Option<u32>,
+    pub overlimits: Option<u32>,
+
+    // XStats
+    pub xstats: Option<XStats>,
+
+    pub bps: Option<u32>,
+    pub pps: Option<u32>,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub enum QDisc {
+    FqCodel(FqCodelQDisc),
+}
+
+impl QDisc {
+    fn new(kind: &str, opts: Vec<TcOption>) -> Option<Self> {
+        if kind == FQ_CODEL {
+            let mut fq_codel = FqCodelQDisc::default();
+            for opt in opts {
+                match opt {
+                    TcOption::FqCodel(fq_codel_opt) => {
+                        match fq_codel_opt {
+                            TcQdiscFqCodelOption::Target(target) => fq_codel.target = target,
+                            TcQdiscFqCodelOption::Limit(limit) => fq_codel.limit = limit,
+                            TcQdiscFqCodelOption::Interval(interval) => fq_codel.interval = interval,
+                            TcQdiscFqCodelOption::Ecn(ecn) => fq_codel.ecn = ecn,
+                            TcQdiscFqCodelOption::Flows(flows) => fq_codel.flows = flows,
+                            TcQdiscFqCodelOption::Quantum(quantum) => fq_codel.quantum = quantum,
+                            TcQdiscFqCodelOption::CeThreshold(ce_threshold) => fq_codel.ce_threshold = ce_threshold,
+                            TcQdiscFqCodelOption::DropBatchSize(drop_batch_size) => fq_codel.drop_batch_size = drop_batch_size,
+                            TcQdiscFqCodelOption::MemoryLimit(memory_limit) => fq_codel.memory_limit = memory_limit,
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            return Some(Self::FqCodel(fq_codel));
+        }
+        None
+    }
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub enum XStats {
+    FqCodel(FqCodelXStats),
+}
+
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct FqCodelQDisc {
+    pub target: u32,
+    pub limit: u32,
+    pub interval: u32,
+    pub ecn: u32,
+    pub flows: u32,
+    pub quantum: u32,
+    pub ce_threshold: u32,
+    pub drop_batch_size: u32,
+    pub memory_limit: u32,
+}
+
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct FqCodelXStats {
+    pub maxpacket: u32,
+    pub drop_overlimit: u32,
+    pub ecn_mark: u32,
+    pub new_flow_count: u32,
+    pub new_flows_len: u32,
+    pub old_flows_len: u32,
+    pub ce_mark: u32,
+    pub memory_usage: u32,
+    pub drop_overmemory: u32,
+}
+
+impl FqCodelXStats {
+    pub fn new(xstats: &TcFqCodelXstats) -> Self {
+        match xstats {
+            TcFqCodelXstats::Qdisc(qdisc) => Self {
+                maxpacket: qdisc.maxpacket,
+                drop_overlimit: qdisc.drop_overlimit,
+                ecn_mark: qdisc.ecn_mark,
+                new_flow_count: qdisc.new_flow_count,
+                new_flows_len: qdisc.new_flows_len,
+                old_flows_len: qdisc.old_flows_len,
+                ce_mark: qdisc.ce_mark,
+                memory_usage: qdisc.memory_usage,
+                drop_overmemory: qdisc.drop_overmemory,
+            },
+            _ => Self::default(),
+        }
+    }
+}
