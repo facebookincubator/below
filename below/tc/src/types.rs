0f448d2e24d91e3263c18f76eadca9bb56616011
mmynk <mohit.ritanil@gmail.com>
2024-02-19 09:26:39 +0000

    Add documentation for public fields

--- below/tc/src/types.rs
+++ below/tc/src/types.rs
@@ -11,12 +11,19 @@ const FQ_CODEL: &str = "fq_codel";
 /// `Tc` represents a traffic control qdisc.
 #[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Tc {
+    /// Index of the network interface.
     pub if_index: u32,
+    /// Name of the network interface.
     pub if_name: String,
+    /// A unique identifier for the qdisc.
     pub handle: u32,
+    /// Identifier of the parent qdisc.
     pub parent: u32,
+    /// Type of the queueing discipline, e.g. `fq_codel`, `htb`, etc.
     pub kind: String,
+    /// Detailed statistics of the qdisc, such as bytes, packets, qlen, etc.
     pub stats: Stats,
+    /// qdisc wraps the specific qdisc type, e.g. `fq_codel`.
     pub qdisc: Option<QDisc>,
 }
 
@@ -81,23 +88,33 @@ impl Tc {
 #[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Stats {
     // Stats2::StatsBasic
+    /// Number of enqueued bytes.
     pub bytes: Option<u64>,
+    /// Number of enqueued packets.
     pub packets: Option<u32>,
 
     // Stats2::StatsQueue
+    /// Length of the queue.
     pub qlen: Option<u32>,
+    /// Number of bytes pending in the queue.
     pub backlog: Option<u32>,
+    /// Packets dropped because of lack of resources.
     pub drops: Option<u32>,
     pub requeues: Option<u32>,
+    /// Number of throttle events when this flow goes out of allocated bandwidth.
     pub overlimits: Option<u32>,
 
     // XStats
+    /// xstats wraps extended statistics of the qdisc.
     pub xstats: Option<XStats>,
 
+    /// Current flow byte rate.
     pub bps: Option<u32>,
+    /// Current flow packet rate.
     pub pps: Option<u32>,
 }
 
+/// `QDisc` represents the queueing discipline of a network interface.
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub enum QDisc {
     FqCodel(FqCodelQDisc),
@@ -143,27 +160,45 @@ pub enum XStats {
 
 #[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct FqCodelQDisc {
+    /// Accceptable minimum standing/persistent queue delay.
     pub target: u32,
+    /// Hard limit on the real queue size.
     pub limit: u32,
+    /// Used to ensure that the measured minimum delay does not become too stale.
     pub interval: u32,
+    /// Used to mark packets instead of dropping them.
     pub ecn: u32,
+    /// Number of flows into which the incoming packets are classified.
     pub flows: u32,
+    /// Number of bytes used as 'deficit' in the fair queuing algorithm.
     pub quantum: u32,
+    /// Sets a threshold above which all packets are marked with ECN Congestion Experienced.
     pub ce_threshold: u32,
+    /// Sets the maximum number of packets to drop when limit or memory_limit is exceeded.
     pub drop_batch_size: u32,
+    /// Sets a limit on the total number of bytes that can be queued in this FQ-CoDel instance.
     pub memory_limit: u32,
 }
 
 #[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct FqCodelXStats {
+    /// Largest packet we've seen so far
     pub maxpacket: u32,
+    /// Number of time max qdisc packet limit was hit.
     pub drop_overlimit: u32,
+    /// Number of packets ECN marked instead of being dropped.
     pub ecn_mark: u32,
+    /// Number of time packets created a 'new flow'.
     pub new_flow_count: u32,
+    /// Count of flows in new list.
     pub new_flows_len: u32,
+    /// Count of flows in old list.
     pub old_flows_len: u32,
+    /// Packets above ce_threshold.
     pub ce_mark: u32,
+    /// Memory usage (bytes).
     pub memory_usage: u32,
+    /// Number of time packets were dropped due to memory limit.
     pub drop_overmemory: u32,
 }
 
