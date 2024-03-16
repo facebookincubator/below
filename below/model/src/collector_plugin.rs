564b43d2c8fe832575695d4a42f887988a24fe33
mmynk <mohit.ritanil@gmail.com>
2024-03-16 18:49:33 -0500

    Revert `rustfmt` change

--- below/model/src/collector_plugin.rs
+++ below/model/src/collector_plugin.rs
@@ -155,14 +155,14 @@ mod test {
             // Test overwriting sample
             futures::executor::block_on(collector.collect_and_update()).unwrap();
             c.wait(); // <-- 1
-                      // Consumer checking overwritten sample
+            // Consumer checking overwritten sample
             c.wait(); // <-- 2
-                      // Test sending None
+            // Test sending None
             futures::executor::block_on(collector.collect_and_update()).unwrap();
             c.wait(); // <-- 3
-                      // Consumer checking None
+            // Consumer checking None
             c.wait(); // <-- 4
-                      // Test sending error. Will fail on both collector and consumer threads.
+            // Test sending error. Will fail on both collector and consumer threads.
             let is_error = matches!(
                 futures::executor::block_on(collector.collect_and_update()),
                 Err(_)
