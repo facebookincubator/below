cc81705d32372ed20faf991413a8ae491e595cea
Brian Chen <brianc118@fb.com>
2024-06-26 13:55:29 -0700

    config: clippy lints
    Differential Revision: D59021480

--- below/config/src/lib.rs
+++ below/config/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![deny(clippy::all)]
+
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
