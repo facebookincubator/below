3a3ff61d03c9df14b88fb760bdfde6734ebd07f4
Brian Chen <brianc118@fb.com>
2024-07-02 13:11:09 -0700

    ethtool: clippy lints + license header
    Differential Revision: D59021484

--- below/ethtool/src/lib.rs
+++ below/ethtool/src/lib.rs
@@ -1,3 +1,18 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![deny(clippy::all)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -89,6 +104,12 @@ fn translate_stats(stats: Vec<(String, u64)>) -> Result<NicStats> {
 }
 
 pub struct EthtoolReader;
+
+impl Default for EthtoolReader {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl EthtoolReader {
     pub fn new() -> Self {
