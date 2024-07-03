f0e14724cbbf5a26517d307f75316c145cb9dfab
Brian Chen <brianc118@meta.com>
2024-07-03 09:51:46 -0700

    gpu_stats: clippy lints
    Reviewed By: boyuni
    
    Differential Revision: D59021483
    
    fbshipit-source-id: f45e33b33fdb31ba1e147fb39df33781e50f57e2

--- below/gpu_stats/src/lib.rs
+++ below/gpu_stats/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![deny(clippy::all)]
+
 use common::open_source_shim;
 
 open_source_shim!(pub);
